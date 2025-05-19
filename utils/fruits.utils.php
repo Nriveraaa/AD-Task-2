9a96cb25ec9496a51e95bc1e88c5746adf8bbdd2
nriveraaa <202310167@fit.edu.ph>
2025-05-20 00:22:45 +0800

    refactor: change the img png file to jpg

--- utils/fruits.utils.php
+++ utils/fruits.utils.php
@@ -1,15 +1,15 @@
 <?php
 function getFruits() {
     return [
-        ["id" => 0, "name" => "Apple", "price" => 1.2, "desc" => "Juicy red apple", "img" => "apple.png"],
-        ["id" => 1, "name" => "Banana", "price" => 0.8, "desc" => "Sweet banana", "img" => "banana.png"],
-        ["id" => 2, "name" => "Orange", "price" => 1.5, "desc" => "Fresh orange", "img" => "orange.png"],
-        ["id" => 3, "name" => "Mango", "price" => 2.0, "desc" => "Tropical ripe mango", "img" => "mango.png"],
-        ["id" => 4, "name" => "Grapes", "price" => 2.5, "desc" => "Seedless sweet grapes", "img" => "grapes.png"],
-        ["id" => 5, "name" => "Pineapple", "price" => 3.0, "desc" => "Freshly sliced pineapple", "img" => "pineapple.png"],
+        ["id" => 0, "name" => "Apple", "price" => 1.2, "desc" => "Juicy red apple", "img" => "apple.jpg"],
+        ["id" => 1, "name" => "Banana", "price" => 0.8, "desc" => "Sweet banana", "img" => "banana.jpg"],
+        ["id" => 2, "name" => "Orange", "price" => 1.5, "desc" => "Fresh orange", "img" => "orange.jpg"],
+        ["id" => 3, "name" => "Mango", "price" => 2.0, "desc" => "Tropical ripe mango", "img" => "mango.jpg"],
+        ["id" => 4, "name" => "Grapes", "price" => 2.5, "desc" => "Seedless sweet grapes", "img" => "grapes.jpg"],
+        ["id" => 5, "name" => "Pineapple", "price" => 3.0, "desc" => "Freshly sliced pineapple", "img" => "pineapple.jpg"],
         ["id" => 6, "name" => "Watermelon", "price" => 4.0, "desc" => "Juicy watermelon slice", "img" => "watermelon.png"],
-        ["id" => 7, "name" => "Strawberry", "price" => 3.5, "desc" => "Sweet red strawberry", "img" => "strawberry.png"],
-        ["id" => 8, "name" => "Blueberry", "price" => 4.5, "desc" => "Organic blueberries", "img" => "blueberry.png"],
+        ["id" => 7, "name" => "Strawberry", "price" => 3.5, "desc" => "Sweet red strawberry", "img" => "strawberry.jpg"],
+        ["id" => 8, "name" => "Blueberry", "price" => 4.5, "desc" => "Organic blueberries", "img" => "blueberry.jpg"],
         ["id" => 9, "name" => "Papaya", "price" => 2.8, "desc" => "Delicious papaya cubes", "img" => "papaya.png"]
     ];
 }
