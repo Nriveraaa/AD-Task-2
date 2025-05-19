05b69d9059799f4094314680a3c45ff4a7bf6788
nriveraaa <202310167@fit.edu.ph>
2025-05-19 23:32:39 +0800

    feat: implement dynamic fruit shop homepage with reusable product card component

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once('utils/fruits.utils.php');
+require_once('components/productCard.component.php');
+
+$fruits = getFruits();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fruit Shop</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>🍓 Fruit Shop</h1>
+    <div class="container">
+        <?php foreach ($fruits as $fruit): ?>
+            <?php echo renderProductCard($fruit); ?>
+        <?php endforeach; ?>
+    </div>
+</body>
+</html>
