71d9db951926ec3043ad8397acc94501f068b965
nriveraaa <202310167@fit.edu.ph>
2025-05-20 08:03:12 +0800

    fix: update fruit prices and descriptions for accuracy

--- utils/fruits.utils.php
+++ utils/fruits.utils.php
@@ -1,16 +1,16 @@
 <?php
 function getFruits() {
     return [
-        ["id" => 0, "name" => "Apple", "price" => 1.2, "desc" => "Juicy red apple", "img" => "apple.jpg"],
-        ["id" => 1, "name" => "Banana", "price" => 0.8, "desc" => "Sweet banana", "img" => "banana.jpg"],
-        ["id" => 2, "name" => "Orange", "price" => 1.5, "desc" => "Fresh orange", "img" => "orange.jpg"],
-        ["id" => 3, "name" => "Mango", "price" => 2.0, "desc" => "Tropical ripe mango", "img" => "mango.jpg"],
-        ["id" => 4, "name" => "Grapes", "price" => 2.5, "desc" => "Seedless sweet grapes", "img" => "grapes.jpg"],
-        ["id" => 5, "name" => "Pineapple", "price" => 3.0, "desc" => "Freshly sliced pineapple", "img" => "pineapple.jpg"],
-        ["id" => 6, "name" => "Watermelon", "price" => 4.0, "desc" => "Juicy watermelon slice", "img" => "watermelon.png"],
-        ["id" => 7, "name" => "Strawberry", "price" => 3.5, "desc" => "Sweet red strawberry", "img" => "strawberry.jpg"],
-        ["id" => 8, "name" => "Blueberry", "price" => 4.5, "desc" => "Organic blueberries", "img" => "blueberry.jpg"],
-        ["id" => 9, "name" => "Papaya", "price" => 2.8, "desc" => "Delicious papaya cubes", "img" => "papaya.png"]
+        ["id" => 0, "name" => "Apple", "price" => 45, "desc" => "Juicy red apples", "img" => "apple.jpg"],
+        ["id" => 1, "name" => "Banana", "price" => 60, "desc" => "Sweet bananas", "img" => "banana.jpg"],
+        ["id" => 2, "name" => "Orange", "price" => 35, "desc" => "Fresh oranges", "img" => "orange.jpg"],
+        ["id" => 3, "name" => "Mango", "price" => 40, "desc" => "Tropical ripe mangoes", "img" => "mango.jpg"],
+        ["id" => 4, "name" => "Grapes", "price" => 80, "desc" => "Seedless sweet grapes", "img" => "grapes.jpg"],
+        ["id" => 5, "name" => "Pineapple", "price" => 80, "desc" => "Fresh pineapples", "img" => "pineapple.jpg"],
+        ["id" => 6, "name" => "Watermelon", "price" => 100, "desc" => "Juicy watermelons", "img" => "watermelon.png"],
+        ["id" => 7, "name" => "Strawberry", "price" => 60, "desc" => "Sweet red strawberries", "img" => "strawberry.jpg"],
+        ["id" => 8, "name" => "Blueberry", "price" => 50, "desc" => "Organic blueberries", "img" => "blueberry.jpg"],
+        ["id" => 9, "name" => "Papaya", "price" => 70, "desc" => "Delicious papayas", "img" => "papaya.png"]
     ];
 }
 ?>
