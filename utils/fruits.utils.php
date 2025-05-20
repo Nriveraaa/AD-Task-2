703a0bcc28b89de738f858e35966470b3c0fb676
nriveraaa <202310167@fit.edu.ph>
2025-05-20 10:21:13 +0800

    refactor: enhance fruit descriptions with additional details for better user information

--- utils/fruits.utils.php
+++ utils/fruits.utils.php
@@ -1,16 +1,86 @@
 <?php
 function getFruits() {
     return [
-        ["id" => 0, "name" => "Apple", "price" => 45, "desc" => "Juicy red apples", "img" => "apple.jpg"],
-        ["id" => 1, "name" => "Banana", "price" => 60, "desc" => "Sweet bananas", "img" => "banana.jpg"],
-        ["id" => 2, "name" => "Orange", "price" => 35, "desc" => "Fresh oranges", "img" => "orange.jpg"],
-        ["id" => 3, "name" => "Mango", "price" => 40, "desc" => "Tropical ripe mangoes", "img" => "mango.jpg"],
-        ["id" => 4, "name" => "Grapes", "price" => 80, "desc" => "Seedless sweet grapes", "img" => "grapes.jpg"],
-        ["id" => 5, "name" => "Pineapple", "price" => 80, "desc" => "Fresh pineapples", "img" => "pineapple.jpg"],
-        ["id" => 6, "name" => "Watermelon", "price" => 100, "desc" => "Juicy watermelons", "img" => "watermelon.png"],
-        ["id" => 7, "name" => "Strawberry", "price" => 60, "desc" => "Sweet red strawberries", "img" => "strawberry.jpg"],
-        ["id" => 8, "name" => "Blueberry", "price" => 50, "desc" => "Organic blueberries", "img" => "blueberry.jpg"],
-        ["id" => 9, "name" => "Papaya", "price" => 70, "desc" => "Delicious papayas", "img" => "papaya.png"]
+        [
+            "id" => 0,
+            "name" => "Apple",
+            "price" => 45,
+            "desc" => "Juicy red apples",
+            "desc2" => "Apples are rich in fiber and antioxidants. Perfect for snacks, juices, or baking.",
+            "img" => "apple.jpg"
+        ],
+        [
+            "id" => 1,
+            "name" => "Banana",
+            "price" => 60,
+            "desc" => "Sweet bananas",
+            "desc2" => "Bananas are high in potassium and great for energy. Ideal for smoothies or as a snack.",
+            "img" => "banana.jpg"
+        ],
+        [
+            "id" => 2,
+            "name" => "Orange",
+            "price" => 35,
+            "desc" => "Fresh oranges",
+            "desc2" => "Oranges are packed with vitamin C. Refreshing and perfect for juicing or a healthy snack.",
+            "img" => "orange.jpg"
+        ],
+        [
+            "id" => 3,
+            "name" => "Mango",
+            "price" => 40,
+            "desc" => "Tropical ripe mangoes",
+            "desc2" => "Mangoes are juicy, sweet, and tropical. Great for desserts, smoothies, or eating fresh.",
+            "img" => "mango.jpg"
+        ],
+        [
+            "id" => 4,
+            "name" => "Grapes",
+            "price" => 80,
+            "desc" => "Seedless sweet grapes",
+            "desc2" => "Grapes are bite-sized fruits rich in antioxidants. Perfect for snacks or making wine.",
+            "img" => "grapes.jpg"
+        ],
+        [
+            "id" => 5,
+            "name" => "Pineapple",
+            "price" => 80,
+            "desc" => "Fresh pineapples",
+            "desc2" => "Pineapples have a sweet and tangy taste. Ideal for grilling, juicing, or desserts.",
+            "img" => "pineapple.jpg"
+        ],
+        [
+            "id" => 6,
+            "name" => "Watermelon",
+            "price" => 100,
+            "desc" => "Juicy watermelons",
+            "desc2" => "Watermelons are hydrating and sweet. Best enjoyed chilled during hot days.",
+            "img" => "watermelon.png"
+        ],
+        [
+            "id" => 7,
+            "name" => "Strawberry",
+            "price" => 60,
+            "desc" => "Sweet red strawberries",
+            "desc2" => "Strawberries are vibrant berries full of vitamin C. Delicious in desserts or eaten fresh.",
+            "img" => "strawberry.jpg"
+        ],
+        [
+            "id" => 8,
+            "name" => "Blueberry",
+            "price" => 50,
+            "desc" => "Organic blueberries",
+            "desc2" => "Blueberries are tiny powerhouses of nutrition. Great for baking or snacking.",
+            "img" => "blueberry.jpg"
+        ],
+        [
+            "id" => 9,
+            "name" => "Papaya",
+            "price" => 70,
+            "desc" => "Delicious papayas",
+            "desc2" => "Papayas aid digestion and are rich in enzymes. Perfect for smoothies or fruit bowls.",
+            "img" => "papaya.png"
+        ]
     ];
 }
 ?>
